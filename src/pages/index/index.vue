134a09aade0a3cb69817449352ae378eeb90ba07
ztt2006 <2743537306@qq.com>
2025-08-03 00:09:58 +0800

    完善轮播图指示点

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,10 @@ import CustomNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar></CustomNavbar>
+  <!-- 自定义轮播图 -->
+  <XtxSwiper></XtxSwiper>
   <view class="index"></view>
 </template>
 
