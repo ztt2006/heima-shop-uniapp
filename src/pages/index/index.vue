d48ea9c5e97fb6f34716ac79bda5b40a68f51009
ztt2006 <2743537306@qq.com>
2025-08-03 03:50:53 +0800

    首页完成

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,10 +2,12 @@
 import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
 import CategoryPanel from './components/CategoryPanel.vue'
+import PageSkeleton from './components/PageSkeleton.vue'
 import { ref } from 'vue'
 import { onLoad } from '@dcloudio/uni-app'
 import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import HotPanel from './components/HotPanel.vue'
+import type { XtxGuessInstance } from '@/types/components'
 const bannerList = ref<BannerItem[]>([])
 // 获取轮播图数据
 const getHomeBannerData = async () => {
@@ -28,27 +30,81 @@ const getHomeHotData = async () => {
   homeHotList.value = res.result
 }
 
-onLoad(() => {
-  getHomeBannerData()
-  getHomeCategoryData()
-  getHomeHotData()
+// 获取猜你喜欢组件实例
+const guessRef = ref<XtxGuessInstance>()
+
+// 滚动到底部事件
+const onScrolltolower = () => {
+  // console.log('滚动到底部')
+  guessRef.value?.getMore()
+}
+
+const isTriggered = ref(false)
+// 下拉刷新事件
+const onRefresherrefresh = async () => {
+  // 开始动画
+  isTriggered.value = true
+  // 加载数据
+  // await getHomeBannerData()
+  // await getHomeCategoryData()
+  // await getHomeHotData()
+  // 重置猜你喜欢组件数据
+  guessRef.value?.resetData()
+  // 并行加载数据
+  await Promise.all([
+    getHomeBannerData(),
+    getHomeCategoryData(),
+    getHomeHotData(),
+    guessRef.value?.getMore(),
+  ])
+
+  // 关闭动画
+  isTriggered.value = false
+}
+
+const isLoading = ref(false)
+onLoad(async () => {
+  isLoading.value = true
+  await Promise.all([getHomeBannerData(), getHomeCategoryData(), getHomeHotData()])
+
+  isLoading.value = false
 })
 </script>
 
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar></CustomNavbar>
-  <!-- 自定义轮播图 -->
-  <XtxSwiper :list="bannerList"></XtxSwiper>
-  <!-- 分类面板 -->
-  <CategoryPanel :list="categoryList"></CategoryPanel>
-  <!-- 热门推荐 -->
-  <HotPanel :list="homeHotList"></HotPanel>
-  <view class="index"></view>
+  <!-- 滚动容器 -->
+  <scroll-view
+    refresher-enabled
+    :refresher-triggered="isTriggered"
+    @refresherrefresh="onRefresherrefresh"
+    @scrolltolower="onScrolltolower"
+    class="scroll-view"
+    scroll-y
+  >
+    <PageSkeleton v-if="isLoading"></PageSkeleton>
+    <template v-else>
+      <!-- 自定义轮播图 -->
+      <XtxSwiper :list="bannerList"></XtxSwiper>
+      <!-- 分类面板 -->
+      <CategoryPanel :list="categoryList"></CategoryPanel>
+      <!-- 热门推荐 -->
+      <HotPanel :list="homeHotList"></HotPanel>
+      <!-- 猜你喜欢 -->
+      <XtxGuess ref="guessRef"></XtxGuess>
+    </template>
+  </scroll-view>
 </template>
 
 <style lang="scss">
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
 }
 </style>
